ca54e73458e3635e625fdc5793c2ba0a69892564
tonywork737 <69871420+tonywork737@users.noreply.github.com>
2021-07-01 10:47:18 +0100

    Add files via upload

--- python/src/video_player.py
+++ python/src/video_player.py
@@ -1,12 +1,12 @@
 """A video player class."""
 
 from .video_library import VideoLibrary
+from .video_playlist import Playlist
 
-
-class Playlist(object):
-    """A class used to represent a Playlist."""
-    name = ' '
-    videos = ' '
+#class Playlist(object):
+    #"""A class used to represent a Playlist."""
+    #name = ' '
+    #videos = ' '
 
 
 class VideoPlayer:
@@ -14,7 +14,9 @@ class VideoPlayer:
 
     def __init__(self):
         self._video_library = VideoLibrary()
-
+        self._video_playlist = Playlist(name=' ', caseless=' ')
+        #self.videoplaying = None
+        self.playlists={}
     def number_of_videos(self):
         num_videos = len(self._video_library.get_all_videos())
         print(f"{num_videos} videos in the library")
@@ -53,14 +55,21 @@ class VideoPlayer:
         """
         videos = self._video_library.get_all_videos()
         counter=0
+        value=3
         for vid in videos:
             if video_id==vid.video_id:
-                counter=1
-                global name
-                name=vid.title
-                global value
-                value=1
-                print(f"Playing video: {name}")
+                if value==3:
+                    counter=1
+                    global name
+                    name=vid.title
+
+                    value=1
+                    print(f"Playing video: {name}")
+                    break
+                elif value==1:
+                    print(f"Stopping video: {name}")
+                    name=vid.title
+                    print(f"Playing video: {name}")
         if counter==0:
             print("Cannot play video: Video does not exist")
             #print("play_video needs implementation")
@@ -131,12 +140,14 @@ class VideoPlayer:
         Args:
             playlist_name: The playlist name.
         """
-
-        global playlist
-        playlist = Playlist()
-        playlist.name = playlist_name
-        print(f"Successfully created new playlist: {playlist.name}")
-
+        #self._video_playlist.name=playlist_name
+        #self._video_playlist.caseless=playlist_name.lower()
+        #print(f"Successfully created new playlist: {self._video_playlist.name}")
+        if playlist_name.lower() not in self.playlists:
+            self.playlists[playlist_name.lower()]=[]
+            print("Successfully created new playlist: {0}".format(playlist_name))
+        else:
+            print("Cannot create playlist: A playlist with the same name already exists")
     def add_to_playlist(self, playlist_name, video_id):
         """Adds a video to a playlist with a given name.
 
@@ -144,29 +155,29 @@ class VideoPlayer:
             playlist_name: The playlist name.
             video_id: The video_id to be added.
         """
+        vid = self._video_library.get_video(video_id)
+        if vid and (playlist_name.lower() in self.playlists):
+            if video_id not in self.playlists[playlist_name.lower()]:
+                print("Added video to {0}: {1}".format(playlist_name, vid.title))
+                self.playlists[playlist_name.lower()].append(video_id)
+            else:
+                print("Cannot add video to {0}: Video already added".format(playlist_name))
+        elif playlist_name not in self.playlists:
+            print("Cannot add video to {0}: Playlist does not exist".format(playlist_name))
+        elif not vid:
+            print("Cannot add video to {0}: Video does not exist".format(playlist_name))
+        #print(f"Added video to {self._video_playlist.name}: {video_id}")
 
-        #import csv
-        #import numpy as np
-        #video_id_list=list(np.loadtxt("/Users/Linda/google-code-sample/python/src/videos.txt")[:, 1])
-        token = open("/Users/Linda/google-code-sample/python/src/videos.txt", 'r')
-        linestoken = token.readlines()
-        tokens_column_number = 1
-        resulttoken = []
-        for x in linestoken:
-            resulttoken.append(x.split()[tokens_column_number])
-        token.close()
-        #with open("/Users/Linda/google-code-sample/python/src/videos.txt") as f:
-            #video_id_list = list(zip(*[line.split() for line in f])[1])
-        if video_id in resulttoken:
-            playlist.videos += f'{video_id}'
-            print(f'Added video to {playlist.name}: {playlist.videos}, {video_id_list}')
-        else:
-            print(f'Cannot add video to [: Video does not exist')
+            #print(f'Added video to {playlist.name}: {playlist.videos}, {video_id_list}')
+        #else:
+            #print(f'Cannot add video to [: Video does not exist')
 
     def show_all_playlists(self):
         """Display all playlists."""
-
-        print("show_all_playlists needs implementation")
+        names=self.playlists.keys()
+        print("Showing all playlists")
+        for item in names:
+            print(item)
 
     def show_playlist(self, playlist_name):
         """Display all videos in a playlist with a given name.
@@ -174,7 +185,26 @@ class VideoPlayer:
         Args:
             playlist_name: The playlist name.
         """
-        print("show_playlist needs implementation")
+        if self.playlists[playlist_name.lower()]!=[]:
+            print(f"Showing playlist: {playlist_name}")
+            for i in self.playlists[playlist_name.lower()]:
+                videos = self._video_library.get_all_videos()
+                templist = []
+
+                def converttostr(input_seq, seperator):
+                    # Join all the strings in list
+                    final_str = seperator.join(input_seq)
+                    return final_str
+
+                for vid in videos:
+                    if i == vid.video_id:
+                        templist.append([vid.title,vid.video_id,vid.tags])
+
+                print(f"  {templist[0][0]} ({templist[0][1]}) [{converttostr(list(templist[0][2]), ' ')}]")
+        else:
+            print(f"Showing playlist: {playlist_name}")
+            print("  No videos here yet")
+        #print("show_playlist needs implementation")
 
     def remove_from_playlist(self, playlist_name, video_id):
         """Removes a video to a playlist with a given name.
@@ -183,7 +213,29 @@ class VideoPlayer:
             playlist_name: The playlist name.
             video_id: The video_id to be removed.
         """
-        print("remove_from_playlist needs implementation")
+        for i in self.playlists[playlist_name.lower()]:
+            videos = self._video_library.get_all_videos()
+            templist = []
+
+            def converttostr(input_seq, seperator):
+                # Join all the strings in list
+                final_str = seperator.join(input_seq)
+                return final_str
+
+            for vid in videos:
+                if i == vid.video_id:
+
+                    temptitle=vid.title
+                    print(f"Removed video from {playlist_name}: {temptitle}")
+                    self.playlists[playlist_name.lower()].remove(video_id)
+
+        if video_id not in self.playlists[playlist_name.lower()]:
+            print("Cannot remove video from my_playlist: Video does not exist")
+        #self.playlists[playlist_name.lower()].remove(video_id)
+
+
+
+        #print("remove_from_playlist needs implementation")
 
     def clear_playlist(self, playlist_name):
         """Removes all videos from a playlist with a given name.
@@ -191,7 +243,12 @@ class VideoPlayer:
         Args:
             playlist_name: The playlist name.
         """
-        print("clears_playlist needs implementation")
+        if playlist_name.lower() in self.playlists:
+            self.playlists[playlist_name.lower()] = []
+            print(f'Successfully removed all videos from {playlist_name}')
+        else:
+            print(f"Cannot clear playlist {playlist_name}: Playlist does not exist")
+
 
     def delete_playlist(self, playlist_name):
         """Deletes a playlist with a given name.
@@ -199,7 +256,11 @@ class VideoPlayer:
         Args:
             playlist_name: The playlist name.
         """
-        print("deletes_playlist needs implementation")
+        if playlist_name.lower() in self.playlists:
+            self.playlists.pop(playlist_name.lower())
+            print(f"Deleted playlist: {playlist_name}")
+        else:
+            print(f"Cannot delete playlist {playlist_name}: Playlist does not exist")
 
     def search_videos(self, search_term):
         """Display all the videos whose titles contain the search_term.
@@ -207,7 +268,27 @@ class VideoPlayer:
         Args:
             search_term: The query to be used in search.
         """
-        print("search_videos needs implementation")
+        videos = self._video_library.get_all_videos()
+
+        temp_list = []
+        for vid in videos:
+
+            # Convoluted way to display tags in required format
+            tags = "["
+            for tag in vid.tags:
+                tags = tags + tag + " "
+            tags = tags + "]"
+            print(f"{vid.title}")
+            if tags != "[]":
+                tags = tags[0:len(tags) - 2] + "]"
+            if str(search_term.lower()) in str(vid.title):
+                temp_list += [f"{vid.title} ({vid.video_id}) {tags}"]
+
+            # Sort the list and display
+        sorted_list = sorted(temp_list)
+        print(f"Here are the results for {search_term}:")
+        for x in sorted_list:
+            print("  " + f"{sorted_list.index(x) + 1}) " + x)
 
     def search_videos_tag(self, video_tag):
         """Display all videos whose tags contains the provided tag.
@@ -215,7 +296,37 @@ class VideoPlayer:
         Args:
             video_tag: The video tag to be used in search.
         """
-        print("search_videos_tag needs implementation")
+        videos = self._video_library.get_all_videos()
+
+        temp_list = []
+        for vid in videos:
+
+            # Convoluted way to display tags in required format
+            tags ="["
+            for tag in vid.tags:
+                tags = tags + tag + " "
+            tags = tags + "]"
+
+            if tags != "[]":
+                tags = tags[0:len(tags)-2] + "]"
+            if video_tag.lower() in tags:
+                temp_list += [f"{vid.title} ({vid.video_id}) {tags}"]
+
+            # Sort the list and display
+        sorted_list = sorted(temp_list)
+        print(f"Here are the results for {video_tag}:")
+        numberlist=[]
+        for x in sorted_list:
+            numberlist.append(sorted_list.index(x)+1)
+            print("  " + f"{sorted_list.index(x)+1}) " + x)
+        print("Would you like to play any of the above? If yes, specify the number of the video.")
+        print("If your answer is not a valid number, we will assume it's a no.")
+
+
+
+
+
+
 
     def flag_video(self, video_id, flag_reason=""):
         """Mark a video as flagged.
@@ -224,7 +335,9 @@ class VideoPlayer:
             video_id: The video_id to be flagged.
             flag_reason: Reason for flagging the video.
         """
-        print("flag_video needs implementation")
+        object=self._video_library.get_video(video_id)
+        
+        print(f"{object}")
 
     def allow_video(self, video_id):
         """Removes a flag from a video.
--- python/src/video_playlist.py
+++ python/src/video_playlist.py
@@ -3,6 +3,7 @@
 
 class Playlist:
     """A class used to represent a Playlist."""
-    def __init__(self,name,videos):
+    def __init__(self,name, caseless, videos = []):
         self.name=name
+        self.caseless=caseless
         self.videos=videos
